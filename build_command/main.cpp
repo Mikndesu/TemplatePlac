bfad2c5f407779a6f515393bba384a661849fc4f
mitsukigoto <mi22kknsinge@ezweb.ne.jp>
2021-03-09 21:59:36 +0900

    modified build command

--- build_command/main.cpp
+++ build_command/main.cpp
@@ -10,7 +10,10 @@ int main(int argc, char *argv[]) {
     } else {
         directory_name = static_cast<std::string>(argv[1]);
     }
+    std::string current_path = static_cast<std::string>(std::filesystem::current_path());
     std::string command =
-    "clang++ " + static_cast<std::string>(std::filesystem::current_path()) + "/" +directory_name +"/main.cpp";
+    "clang++ " + current_path + "/" +directory_name +"/main.cpp";
+    std::system(command.c_str());
+    command = "mv " + current_path +"/a.out " + current_path + "/" +directory_name +"/";
     std::system(command.c_str());
 }
--- proj1/main.cpp
+++ proj1/main.cpp
@@ -1,5 +1,16 @@
 // refer to http://www.nowhere.co.jp/blog/archives/20160728-230519.html
 
+#include <iostream>
+
+template<int N> struct Factorial{
+    enum{ value = N * Factorial<N-1>::value};
+};
+
+template<> struct Factorial<0> {
+    enum{ value = 1};
+};
+
 int main() {
+    std::cout << Factorial<5>::value << std::endl;
     return 0;
 }
